a0d781b60f7628e7450e5b211b86a95356964e90
tallpsmith <psmith@aconex.com>
2017-04-09 08:07:21 +1000

    use ui.grid instead of simple table.

--- protractor/fuelcalculator-spec.js
+++ protractor/fuelcalculator-spec.js
@@ -11,8 +11,11 @@ describe('FuelCalculator By Lap', function() {
     element(by.model('consumption')).sendKeys('0.605');
 
 
-    var pitTable = element.all(by.repeater('pit in pit'));
-    expect(pitTable.count()).toEqual(5);
+    // really need to use the gridTest but not sure how to include that library in there yett..
+    // see http://ui-grid.info/docs/#/tutorial/403_end_to_end_testing
+    // I've copied this from the test utility itself for now.
+    var rows = element(by.id('pitStopGrid')).element( by.css('.ui-grid-render-container-body')).all( by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index') );
+    expect(rows.count()).toEqual(5);
 
     expect(element(by.binding('lapsinsession')).getText()).toBe('Your race should be 20 laps');
     expect(element(by.binding('fuelneededinsession')).getText()).toBe('You need 12.100 units of fuel');
