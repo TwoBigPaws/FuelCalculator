719a41a23e061efa0e3f23330f26944ec6351888
tallpsmith <psmith@aconex.com>
2017-04-08 12:40:53 +1000

    Test that the consumption field is displayed when switching to the byLap tab and remove commented out come

--- protractor/fuelcalculator-spec.js
+++ protractor/fuelcalculator-spec.js
@@ -4,6 +4,8 @@ describe('FuelCalculator By Lap', function() {
 
     element(by.id('byLapTab')).click();
 
+    element(by.model('consumption')).isDisplayed();
+
     element(by.model('totaltanksize')).clear().sendKeys('3');
     element(by.model('numberOfLaps')).sendKeys('20');
     element(by.model('consumption')).sendKeys('0.605');
@@ -14,9 +16,5 @@ describe('FuelCalculator By Lap', function() {
 
     expect(element(by.binding('lapsinsession')).getText()).toBe('Your race should be 20 laps');
     expect(element(by.binding('fuelneededinsession')).getText()).toBe('You need 12.100 units of fuel');
-    //expect(element.all(by.id('CalculatedFuelRequirement')).getAttribute('value')).toEqual('You need 12.100 units of fuel');
-
-    //browser.pause();
-
   });
 });
