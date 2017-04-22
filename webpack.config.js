78c357ccb48fab0c84783320a8f6ad9bc34158d8
tallpsmith <psmith@aconex.com>
2017-04-23 09:47:20 +1000

    webpack: change webpack config to do all that CSS loading thing.

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,19 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({name: "vendor", filename: "vendor.bundle.js"})
-  ]
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.css$/, // Only .css files
+        loader: 'style-loader!css-loader' // Run both loaders
+      },
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
+
+    ]
+  }
 };
 
