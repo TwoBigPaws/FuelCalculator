4ff042b286a411d9829a208d9acf12f64513c5f9
tallpsmith <psmith@aconex.com>
2017-04-22 14:25:27 +1000

    webpack: Change the SimplePitStopStrategy to be a Factory pattern

--- app/services/simple-pitstop-strategy.js
+++ app/services/simple-pitstop-strategy.js
@@ -1,21 +1,27 @@
-function SimplePitStopStrategy(consumption, minimumFuel, maximumFuel) {
+"use strict";
+
+function SimplePitStopStrategy() {
   return {
-    consumption: consumption,
-    minimumFuel: minimumFuel,
-    maximumFuel: maximumFuel,
-    shouldPit: function (lapData) {
-      // we need to make sure that we would consume fuel below minimumFuel
-      // when the engine starts coughing
-      var wouldBeBelowMinimum = (lapData.fuelState - this.consumption) < this.minimumFuel;
-      if (wouldBeBelowMinimum && lapData.lapsRemaining > 0) {
-        return true;
-      }
-      return false;
-    },
-    fuelLevelToFillTo: function (lapData) {
-      return this.maximumFuel;
+    newStrategy: function (consumption, minimumFuel, maximumFuel) {
+      return {
+        consumption: consumption,
+        minimumFuel: minimumFuel,
+        maximumFuel: maximumFuel,
+        shouldPit: function (lapData) {
+          // we need to make sure that we would consume fuel below minimumFuel
+          // when the engine starts coughing
+          var wouldBeBelowMinimum = (lapData.fuelState - this.consumption) < this.minimumFuel;
+          if (wouldBeBelowMinimum && lapData.lapsRemaining > 0) {
+            return true;
+          }
+          return false;
+        },
+        fuelLevelToFillTo: function (lapData) {
+          return this.maximumFuel;
+        }
+      };
     }
   };
-};
+}
 
 module.exports = SimplePitStopStrategy;
