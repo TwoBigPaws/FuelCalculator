25ad63ad124f4cf997c77aa22d5a804a93f7222b
tallpsmith <psmith@aconex.com>
2017-04-23 08:31:39 +1000

    webpack: Bring back angular and angular mocks into karma configuration.
    seems to be needed.  I get why angular-mocks is needed, but not why angular as well..

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
       'js/vendor*.js',
       'js/app*.js',
       'test/*.js'
