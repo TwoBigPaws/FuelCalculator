03f55b5e1f99a4f22261ca35422bb91ca766336a
tallpsmith <psmith@aconex.com>
2017-04-24 18:00:56 +1000

    reorder the list of requires here so that things come out correctly, particularly including declared dependency of CalcApp -> CalcCtrl which resolves some missing bits.
    Thanks Bulks.

--- app/app.js
+++ app/app.js
@@ -1,16 +1,17 @@
 "use strict"
 
 var angular = require("angular");
-angular.module("CalcApp", ['ui.bootstrap','FuelCalculators','FuelTank','Strategies', 'ui.grid','chart.js', 'angularValidator']);
 
-require("./controllers");
-require("./services");
 require("../node_modules/angular-ui-bootstrap");
 require("../node_modules/chart.js");
 require("../node_modules/angular-chart.js");
 require("../node_modules/angular-ui-grid");
+require("../node_modules/angular-ui-grid/ui-grid.css");
 require("../node_modules/angular-validator");
-require("bootstrap-webpack");
-require("../node_modules/angular-ui-grid/ui-grid");
+var controllers = require("./controllers");
+require("./services");
 
+require("bootstrap-webpack");
+
+angular.module("CalcApp", ['ui.bootstrap', controllers, 'ui.grid','chart.js', 'angularValidator']);
 
