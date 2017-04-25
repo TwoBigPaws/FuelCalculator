886943ff392e5173dbe3110061aac18417f4543a
tallpsmith <psmith@aconex.com>
2017-04-25 11:20:55 +1000

    webpack: do not Uglify explicitly, let the webpack -p do that for us.

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,7 @@ module.exports = {
     filename: 'app.bundle.js'
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({name: "vendor", filename: "vendor.bundle.js"}),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.CommonsChunkPlugin({name: "vendor", filename: "vendor.bundle.js"})
   ],
   module: {
     loaders: [
